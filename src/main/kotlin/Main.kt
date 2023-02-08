74492c232898da636b5cce0205bf5fe1a930d8b0
JecintaNjoki <jecintanjokikimanii@gmail.com>
2023-02-08 19:55:07 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,29 @@
+fun main (){
+
+greetings("Jecinta")
+modulus(num1 :Int,num2:Int)
+    addition()
+}
+
+
+
+
+fun greetings (name: String) {
+  var name =("Hello +name)
+prrintln(name)
+
+}
+
+
+fun modulus(num1 : Int,num2 : Int) {
+
+    var num1 = 78
+    var num2 = 22
+    var remainder = num1 % num2
+    println(remainder)
+
+    fun adiition(num1 :Int)
+   var num1= 23+55
+    var num2=40+77
+    var sum= num1+num2
+    println(sum)
